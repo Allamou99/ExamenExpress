8c26cee3c36f72863774522e7f2624e7361719d4
Soufiane <soufianeallamou99@outlook.fr>
2021-12-03 15:43:15 +0100

    added prod env

--- index.js
+++ index.js
@@ -5,18 +5,19 @@ var vmsRouter = require('./Routes/VMrouter');
 const mongoose = require('mongoose');
 var cors = require('cors')
 var configDev = require('./dev');
+var configProd = require('./prod');
 app.use(cors());
-
-const connect = mongoose.connect(configDev.link);
+var link = process.env.MONGODB_URI;
+const connect = mongoose.connect(link);
 connect.then((db)=>{
-  console.log('Connected to local mongodb Database');
+  console.log('Connected to atlas mongodb Database');
 }, (err)=>{console.log(err); });
 
 
 app.use('/vms', vmsRouter);
 
 const port = 3000;
-const hostname = 'localhost'
+const hostname='0.0.0.0';
 var server = http.createServer(app)
 server.listen(port ,hostname,()=>{
     console.log('Server is running on port ' + port);
---
+++ prod.js
@@ -0,0 +1,3 @@
+module.exports = {
+    link:'mongodb+srv://soufiane99:Dimahmd123@cluster0.7d6e8.mongodb.net/VmsApp?retryWrites=true&w=majority'
+}
